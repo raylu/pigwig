2053714730edba6ffd46cc9e78d43d5d3fb615aa
jmw327 <josh@jmw327.com>
2015-05-29 20:31:15 -0700

    add set cookie method

--- pigwig/request_response.py
+++ pigwig/request_response.py
@@ -19,6 +19,23 @@ class Response:
 		self.headers = copy.copy(self.DEFAULT_HEADERS)
 		self.headers.append(('Content-Type', self.content_type))
 
+	def set_cookie(self, key, value, domain=None, path=None, expires=None, max_age=None, secure=False, http_only=False):
+		cookie = '%s=%s' % (key, value)
+		if domain:
+			cookie += '; Domain=%s' % domain
+		if path:
+			cookie += '; Path=%s' % path
+		if expires:
+			expires = expires.strftime('%a, %d %b %Y %H:%M:%S GMT')
+			cookie += '; Expires=%s' % expires
+		if max_age:
+			cookie += '; Max-Age=%d' % max_age
+		if secure:
+			cookie += '; Secure'
+		if http_only:
+			cookie += '; HttpOnly'
+		self.headers.append(('Set-Cookie', cookie))
+
 	@classmethod
 	def render(cls, request, template, context):
 		body = request.app.template_engine.render(template, context)
