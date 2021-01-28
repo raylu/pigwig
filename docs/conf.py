48ea351780d3eac54c167149ddab2835b60bd660
raylu <90059+raylu@users.noreply.github.com>
2021-01-27 17:06:05 -0800

    bump docs version to 0.7

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,9 @@ copyright = 'raylu'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.7'
 # The full version, including alpha/beta/rc tags.
-release = '0.1'
+release = '0.7'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
