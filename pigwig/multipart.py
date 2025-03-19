3f090fb3efd4d2c6e7bbaa7e8d17fa2601a837aa
raylu <90059+raylu@users.noreply.github.com>
2025-03-18 22:46:57 -0700

    fix multipart.py indentation

--- pigwig/multipart.py
+++ pigwig/multipart.py
@@ -87,35 +87,35 @@ def parse_multipart(fp: io.BufferedIOBase, pdict: dict) -> dict[str, list[bytes 
 	return partdict
 
 def parse_header(line: str) -> tuple[str, dict[str, str]]:
-    """Parse a Content-type like header.
+	"""Parse a Content-type like header.
 
-    Return the main content-type and a dictionary of options.
-    """
-    parts = _parseparam(';' + line)
-    key = parts.__next__()
-    pdict = {}
-    for p in parts:
-        i = p.find('=')
-        if i >= 0:
-            name = p[:i].strip().lower()
-            value = p[i+1:].strip()
-            if len(value) >= 2 and value[0] == value[-1] == '"':
-                value = value[1:-1]
-                value = value.replace('\\\\', '\\').replace('\\"', '"')
-            pdict[name] = value
-    return key, pdict
+	Return the main content-type and a dictionary of options.
+	"""
+	parts = _parseparam(';' + line)
+	key = parts.__next__()
+	pdict = {}
+	for p in parts:
+		i = p.find('=')
+		if i >= 0:
+			name = p[:i].strip().lower()
+			value = p[i+1:].strip()
+			if len(value) >= 2 and value[0] == value[-1] == '"':
+				value = value[1:-1]
+				value = value.replace('\\\\', '\\').replace('\\"', '"')
+			pdict[name] = value
+	return key, pdict
 
 def _parseparam(s: str) -> typing.Iterator[str]:
-    while s[:1] == ';':
-        s = s[1:]
-        end = s.find(';')
-        while end > 0 and (s.count('"', 0, end) - s.count('\\"', 0, end)) % 2:
-            end = s.find(';', end + 1)
-        if end < 0:
-            end = len(s)
-        f = s[:end]
-        yield f.strip()
-        s = s[end:]
+	while s[:1] == ';':
+		s = s[1:]
+		end = s.find(';')
+		while end > 0 and (s.count('"', 0, end) - s.count('\\"', 0, end)) % 2:
+			end = s.find(';', end + 1)
+		if end < 0:
+			end = len(s)
+		f = s[:end]
+		yield f.strip()
+		s = s[end:]
 
 class MultipartFile:
 	"""
